bffb2ec716f5944756a46d9fe82e1048b51c9c95
EduOliveira29 <realmadri123@yahoo.com.br>
2025-04-15 08:21:49 -0300

    first commit

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Post.module.css';
+
+import PostComments from '../PostComments';
+import { ReactNode } from 'react';
+
+type Props = {
+    children: ReactNode;
+}
+
+const Post = ({ children }: Props) => (
+    <div className={styles.post}>
+        <p className={styles['post-text']}> {children} </p>
+        <PostComments />
+    </div>
+);
+
+export default Post;
---
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -0,0 +1,43 @@
+import {fireEvent, render, screen } from '@testing-library/react';
+import Post from '../../../App'
+import PostComment from '../index'
+
+describe('Teste para o componente Post', () => {
+    test('Deve renderizar o componente corretamente', () => {
+        render(<Post />);
+        expect(screen.getByText('Olha só que legal minha miniatura do Batmóvel.')).toBeInTheDocument();
+    });
+});
+
+describe('Teste para o Primeiro comentario', () => {
+    test('Deve retornar o valor digitado', () =>  {
+        const { debug } = render(<PostComment  />);
+
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Primeiro comentario'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+        // eslint-disable-next-line testing-library/no-debugging-utils
+        debug()
+        expect(screen.getByText('Primeiro comentario')).toBeInTheDocument()
+    });
+});
+
+describe('Teste para o Segundo comentario', () => {
+    test('Deve retornar o valor digitado', () =>  {
+        const { debug } = render(<PostComment  />);
+
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Segundo comentario'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+        // eslint-disable-next-line testing-library/no-debugging-utils
+        debug()
+        expect(screen.getByText('Segundo comentario')).toBeInTheDocument()
+    });
+});
+
